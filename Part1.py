24f1c3058af900b68e871f5b4843e5a694533f3e
faun <915523122@qq.com>
2021-08-22 17:10:14 +1000

    update

--- Part1.py
+++ Part1.py
@@ -1,13 +1,16 @@
 from moviepy.editor import *
 
-video = VideoFileClip('1.mov')
-audio = video.audio
-audio.write_audiofile('a.mp3')
+ac = AudioFileClip('5.mp4')
+ac.write_audiofile('a.wav',fps=16000, ffmpeg_params=["-ac", "1"])
+
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/faun/PycharmProjects/CineflyProject/key.json"
 
 def transcribe_model_selection(speech_file, model):
     """Transcribe the given audio file synchronously with
     the selected model."""
-    import google.cloud.speech as speech
+
+    from google.cloud import speech_v1p1beta1 as speech
 
     client = speech.SpeechClient()
 
@@ -20,15 +23,45 @@ def transcribe_model_selection(speech_file, model):
         encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
         sample_rate_hertz=16000,
         language_code="en-US",
+        enable_automatic_punctuation=True,
         model=model,
+
     )
 
     response = client.recognize(config=config, audio=audio)
+
+    strALl=''
 
     for i, result in enumerate(response.results):
         alternative = result.alternatives[0]
         print("-" * 20)
         print("First alternative of result {}".format(i))
-        print(u"Transcript: {}".format(alternative.transcript))
+        strALl+=u"Transcript: {}".format(alternative.transcript);
 
-transcribe_model_selection('a.mp3',"default")
+    return strALl;
+
+
+trans=transcribe_model_selection('a.wav',"default")
+
+print(trans)
+
+import boto3
+
+comprehend = boto3.client('comprehend',
+                              region_name = "us-east-2",
+                              aws_access_key_id="AKIA4LSCBOB7257IG4F3",
+                              aws_secret_access_key="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")
+phrases = comprehend.detect_pii_entities(Text=trans, LanguageCode="en")
+# for i in range(0,len(phrases['Entities'])):
+#     print((phrases['KeyPhrases'][i]['Text']))
+# print(phrases)
+
+entities = phrases['Entities']
+
+# print(entities[0]['Type'])
+with open('new.csv', 'w') as f:
+    for entity in entities:
+        f.write(str(entity['Type']))
+        f.write(" : ")
+        f.write(trans[entity['BeginOffset']:entity['EndOffset']])
+        f.write("\n")

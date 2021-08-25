1025eff15f102043b31dab9e387193021e90215d
yixian qiu <u6935151@anu.edu.au>
2021-08-25 18:39:02 +1000

    fix run_demo_2

--- Run_Demo_2.py
+++ Run_Demo_2.py
@@ -1,7 +1,7 @@
 from DemoTestVersion import run_demo_2
 import csv
 
-path=r"1"
+path=r"voiceover"
 path_intro=r"into"
 
 fileName_List,vText_List,gender_List,age_List,amazonDir_List=run_demo_2(path,path_intro)
@@ -14,18 +14,26 @@ with open('PersonInfo.csv', 'w', encoding='UTF-8', newline="") as f:
         name=""
 
         thisList=amazonDir_List[i]
+        print(thisList)
+        for j in range(len(thisList)):
+            thisDir=thisList[j]
+            print(thisDir)
+            print(type(thisDir))
+            if type(thisDir)==type(""):
+                break
+            else:
+                if "AGE" in thisDir.keys():
+                    age_List[i]="("+thisDir["AGE"]+")"
+                    break
 
         for j in range(len(thisList)):
             thisDir=thisList[j]
-            if "AGE" in thisDir.keys():
-                age_List[i]="("+thisDir["AGE"]+")"
+            if type(thisDir) == type(""):
                 break
-
-        for j in range(len(thisList)):
-            thisDir=thisList[j]
-            if "NAME" in thisDir.keys():
-                name=thisDir["NAME"]
-                break
+            else:
+                if "NAME" in thisDir.keys():
+                    name=thisDir["NAME"]
+                    break
         csv.writer(f).writerow([fileName_List[i],name,age_List[i],gender_List[i],vText_List[i]])
     # for i in range(len(allEntity)):
     #     csv.writer(f).writerow([allEntity[i]['fileName'],allEntity[i]['gender'],allEntity[i]['age'],allEntity[i]['vText']])

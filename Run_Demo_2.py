69154d4b2d1fe357ce52e1d9b1ead300f76ba232
Tao Qu <71800127+CrazyTodd@users.noreply.github.com>
2021-08-25 23:40:41 +0800

    Update Run_Demo_2.py
    Function to add additional tags and values

--- Run_Demo_2.py
+++ Run_Demo_2.py
@@ -6,8 +6,29 @@ path_intro= r"intro"
 
 fileName_List,vText_List,gender_List,age_List,amazonDir_List=run_demo_2(path,path_intro)
 
-with open('PersonInfo.csv', 'w', encoding='UTF-8', newline="") as f:
-    csv.writer(f).writerow(["fileName","name","age","gender","vText"])
+
+
+headers = ["fileName","name","age","gender","vText"]
+newHeaders = []
+
+for entityList in amazonDir_List:
+    for i in range(len(entityList)):
+        for j in list(entityList[i].keys()):
+            if (not j in headers) and (not j in newHeaders):
+                newHeaders.append(j)
+
+with open('PersonInfo.csv','w',newline='') as f:
+    writer = csv.DictWriter(f,fieldnames=headers+newHeaders)
+    writer.writeheader()
+f.close
+
+
+
+with open('PersonInfo.csv', 'a', encoding='UTF-8', newline="") as f:
+    
+    writer = csv.DictWriter(f, fieldnames=headers+newHeaders)
+    
+    # csv.writer(f).writerow(["fileName","name","age","gender","vText"])
     # for entity in allEntity
     #     csv.writer(f).writerow([entity['fileName'],entity['vText'],entity['gender'],entity['age']])
     for i in range(len(fileName_List)):
@@ -32,7 +53,19 @@ with open('PersonInfo.csv', 'w', encoding='UTF-8', newline="") as f:
                 if "NAME" in thisDir.keys():
                     name=thisDir["NAME"]
                     break
-        csv.writer(f).writerow([fileName_List[i],name,age_List[i],gender_List[i],vText_List[i]])
+                    
+                    
+                    
+        
+        dictValue = {'fileName':fileName_List[i],'name':name,'age':age_List[i],'gender':gender_List[i],'vText':vText_List[i]}
+        
+        for j in range(len(thisList)):
+            thisDir=thisList[j]
+            for key in thisDir.keys():
+                if key in newHeaders:
+                    dictValue.update({key:thisDir[key]})
+                    break
+        write.writerow(dictValue)
     # for i in range(len(allEntity)):
     #     csv.writer(f).writerow([allEntity[i]['fileName'],allEntity[i]['gender'],allEntity[i]['age'],allEntity[i]['vText']])
     f.close

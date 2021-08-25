4d382b864471faf1c2e6d52721f547847655c5c8
faun <915523122@qq.com>
2021-08-25 11:46:21 +1000

    Demo v1.0

--- Demo.py
+++ Demo.py
@@ -1,12 +1,16 @@
+## Use the MoviePy package to extract the audio from the video into a required format.
 from moviepy.editor import *
 
-fps=44100;
+fps=44100;  # Set the audio bit rate, 44100Hz is a common high-fidelity MP3 standard.
+# If you need to reduce the bandwidth usage, you can reduce this value, but it may affect the accuracy.
 
 ac = AudioFileClip('S.mp4')
 ac.write_audiofile('Audio.wav',fps=fps, ffmpeg_params=["-ac", "1"])
 
+
+## Call Google API
 import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "GCKey.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "GCKey.json" # Recall the Google key stored locally
 
 def transcribe_model_selection(speech_file, model):
     """Transcribe the given audio file synchronously with
@@ -43,12 +47,15 @@ def transcribe_model_selection(speech_file, model):
     return strALl;
 
 
-trans=transcribe_model_selection('Audio.wav',"default")
+trans=transcribe_model_selection('Audio.wav',"default")  # Call the above function and get the result.
 
 print(trans)
 
+
+# Call Amazon API
 import boto3
 
+#Amazon Key config
 comprehend = boto3.client('comprehend',
                               region_name = "us-east-2",
                               aws_access_key_id="AKIA4LSCBOB7257IG4F3",
@@ -60,7 +67,8 @@ phrases = comprehend.detect_pii_entities(Text=trans, LanguageCode="en")
 
 entities = phrases['Entities']
 
-# print(entities[0]['Type'])
+
+#Save result in 'RstInfo.csv'
 with open('RstInfo.csv', 'w') as f:
     for entity in entities:
         f.write(str(entity['Type']))
